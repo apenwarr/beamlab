c32298caad7dbd9914803da0f441e33a0e382204
Avery Pennarun <apenwarr@gmail.com>
2014-07-21 07:26:32 +0100

    Wrong, but convincing looking, interference patterns.

--- wavemap.js
+++ wavemap.js
@@ -2,39 +2,101 @@
 
 var pluscolor = new Uint8ClampedArray([0,128,255,255]);
 var minuscolor = new Uint8ClampedArray([255,64,64,255]);
-var blackcolor = new Uint8ClampedArray([0,0,0,255]);
-
 
 var xsize = 1000, ysize = 1000;
 var canvas = document.getElementById('field');
-
 canvas.width = xsize;
 canvas.height = ysize;
 var ctx = canvas.getContext('2d');
 var img = ctx.createImageData(xsize, ysize);
 
-var areabuf = new ArrayBuffer(4 * xsize * ysize);
-var area = new Float32Array(areabuf);
 
-for (var i = 0; i < area.length; i++) {
-  area[i] = Math.sin(i/xsize*10.005 * 2 * Math.PI);
+var px_per_m = 40; // pixels per meter
+var dB_max = 30, dB_min = -60;
+var wavelength_m = 3e8 / 2.4e9;
+
+
+function pointSource(x0, y0, phase0, power_1m) {
+  var areabuf = new ArrayBuffer(4 * xsize * ysize);
+  var area = new Float32Array(areabuf);
+
+  for (var i = 0; i < area.length; i++) {
+    var y = i / xsize;
+    var x = i % xsize;
+    var dy = (y - y0) / px_per_m;
+    var dx = (x - x0) / px_per_m;
+    var r = Math.sqrt(dy*dy + dx*dx);
+    var phase = (phase0 + r / wavelength_m) % (2 * Math.PI);
+    area[i] = power_1m / (r * r);
+    if (phase > Math.PI) area[i] = -area[i];
+  }
+  return area;
 }
 
-function setPix(img, val) {
+
+// TODO: this is WRONG!  Must properly add using phases.
+function addAreas(areas) {
+  var areabuf = new ArrayBuffer(4 * xsize * ysize);
+  var area = new Float32Array(areabuf);
+
+  for (var ai = 0; ai < areas.length; ai++) {
+    var a = areas[ai];
+    for (var i = 0; i < a.length; i++) {
+      area[i] += a[i];
+    }
+  }
+  return area;
+}
+
+
+function setPix(img, i, val) {
   if (val >= 0) {
     var c = pluscolor;
+    var dB = 10*Math.log(val, 10);
   } else {
     var c = minuscolor;
-    val = -val;
+    var dB = 10*Math.log(-val, 10);
   }
+  var scale = (dB - dB_min) / (dB_max - dB_min);
+  if (scale < 0) scale = 0;
   for (var j = 0; j < 3; j++) {
-    img.data[4*i + j] = c[j] * val;
+    img.data[4*i + j] = c[j] * scale;
   }
   img.data[4*i + 3] = 255;
 }
 
-for (var i = 0; i < area.length; i++) {
-  setPix(img, area[i]);
+
+var cx = 300, cy = 400;
+var a1 = pointSource(300, 300, 0, 1);
+
+function render() {
+  var a2 = pointSource(cx, cy, Math.PI, 1);
+  var area = addAreas([a1, a2]);
+
+  for (var i = 0; i < area.length; i++) {
+    setPix(img, i, area[i]);
+  }
+  ctx.putImageData(img, 0, 0);
 }
 
-ctx.putImageData(img, 0, 0);
+render();
+var rendering = 0;
+
+canvas.onmousemove = function(e) {
+  if (rendering) {
+    console.debug(e);
+    cx = e.x / canvas.clientWidth * xsize;
+    cy = e.y / canvas.clientHeight * ysize;
+    render();
+  }
+}
+
+canvas.onmousedown = function(e) {
+  rendering = 1;
+  canvas.onmousemove(e);
+}
+
+canvas.onmouseup = function(e) {
+  rendering = 0;
+}
+
